1d310b5f565b162b484954645b30637bebdc7c6f
developerdarshana123 <23115667@hitdoon.com>
2025-03-24 20:10:13 +0530

    JS EVENTS

---
+++ darshii.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            document.body.style.backgroundColor = this.id;
+        });
+    });
+});
