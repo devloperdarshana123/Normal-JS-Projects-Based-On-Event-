f03889be46ee6697cdc730448993e26cea463773
developerdarshana123 <23115667@hitdoon.com>
2025-03-26 21:00:08 +0530

    Promise Learning using then or catch method also async await also but here we use try and catch

---
+++ promise.js
@@ -0,0 +1,75 @@
+// const console = require("console");
+
+// const promiseone = new Promise(function (resolve , reject){
+// setTimeout(function(){
+//     console.log("async task is completed");
+//     resolve()
+// } , 1000)
+// })
+
+// promiseone.then(function(){
+//     console.log("task is completed Promise is Consume")
+// })
+
+// new Promise( function(resolve , reject) {
+//     setTimeout(function(){
+//         console.log("Async task two ");
+//         resolve()
+// }, 1000)
+// }).then(function(){
+//     console.log("Task is completed Promise 2  is Consume");
+// })
+// const  promisethree = new Promise(function(resolve , reject){
+//     setTimeout(function(){
+// resolve({username: "Darshi" , email:"chai@example.com" , age: "19"})
+//     } , 1000)
+// })
+// promisethree.then(function(user){
+// console.log(user)
+// })
+
+// const promisefour = new Promise(function(resolve , reject){
+//     setTimeout(function(){
+//         let error = false
+//         if(!error){
+// resolve({username:"darshii"  , love:"Rohii"})
+//         }
+//         else{
+//             reject("Error:Something went wrong")
+//         }
+//     } , 1000)
+// })
+//  promisefour
+//  .then((user)=>{
+// console.log(user);
+// return user.username
+// })
+// .then((username)=>{
+// console.log(username);
+// })
+// .catch(function(error){
+//     console.log(error);
+// }).finally(()=> console.log("the promise is resolve "))
+
+const promisefive = new Promise(function(resolve , reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+resolve({username:"darshiiRohii"  , love:"RohiiDarshii"})
+        }
+        else{
+            reject("Error:Something went wrong in username")
+        }
+    } , 1000)
+});
+async function consumep(){
+    try{
+        const response = await promisefive
+        console.log(response); 
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+consumep()
+
